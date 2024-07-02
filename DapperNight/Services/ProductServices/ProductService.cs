57a5d9ce33894c1f06ea77993bd3690962def519
Emre Keskiner <emrekeskiner@gmail.com>
2024-07-02 20:30:39 +0300

    GetByIdProductAsync metodunda oluşabilecek bir hata giderildi.

--- DapperNight/Services/ProductServices/ProductService.cs
+++ DapperNight/Services/ProductServices/ProductService.cs
@@ -15,7 +15,7 @@ namespace DapperNight.Services.ProductServices
 
         public async Task CreateProductAsync(CreateProductDto createProductDto)
         {
-            var query = "insert into TblProduct (ProductName,Stock,Price,CategoryId) values (@productName,@stock,@price,@categoryId)";
+            string query = "insert into TblProduct (ProductName,Stock,Price,CategoryId) values (@productName,@stock,@price,@categoryId)";
             var parameters = new DynamicParameters();
             parameters.Add("@productName",createProductDto.ProductName);
             parameters.Add("@stock", createProductDto.Stock);
@@ -27,7 +27,7 @@ namespace DapperNight.Services.ProductServices
 
         public async Task DeleteProductAsync(int id)
         {
-            var query = "Delete From TblProduct Where ProductId=@productId";
+            string query = "Delete From TblProduct Where ProductId=@productId";
             var parameters = new DynamicParameters();
             parameters.Add("@productId", id);
             var connection = _context.CreateConnection();
@@ -36,7 +36,7 @@ namespace DapperNight.Services.ProductServices
 
         public async Task<List<ResultProductDto>> GetAllProductAsync()
         {
-            var query = "Select ProductId,ProductName,CategoryName,Stock,Price From TblProduct inner join TblCategory on TblProduct.CategoryId=TblCategory.CategoryId";
+            string query = "Select ProductId,ProductName,CategoryName,Stock,Price From TblProduct inner join TblCategory on TblProduct.CategoryId=TblCategory.CategoryId";
             var connection = _context.CreateConnection();
             var values = await connection.QueryAsync<ResultProductDto>(query);
             return values.ToList();
@@ -44,17 +44,17 @@ namespace DapperNight.Services.ProductServices
 
         public async Task<GetByIdProductDto> GetByIdProductAsync(int id)
         {
-            var query = "Select ProductId, ProductName,TblProduct.CategoryId,CategoryName,Stock,Price From TblProduct inner join TblCategory on TblProduct.CategoryId=TblCategory.CategoryId Where ProductId=@productId";
+            string query = "Select ProductId, ProductName,TblProduct.CategoryId,CategoryName,Stock,Price From TblProduct inner join TblCategory on TblProduct.CategoryId=TblCategory.CategoryId Where ProductId=@productId";
             var parameters = new DynamicParameters();
             parameters.Add("@productId", id);
             var connection = _context.CreateConnection();
-            var value = await connection.QueryFirstAsync<GetByIdProductDto>(query,parameters);
+            var value = await connection.QueryFirstOrDefaultAsync<GetByIdProductDto>(query,parameters);
             return value;
         }
 
         public async Task UpdateProductAsync(UpdateProductDto updateProductDto)
         {
-            var query = "Update TblProduct Set ProductName=@productName,Stock=@stock,Price=@price,CategoryId=@categoryId Where ProductId=@productId";
+            string query = "Update TblProduct Set ProductName=@productName,Stock=@stock,Price=@price,CategoryId=@categoryId Where ProductId=@productId";
             var parameters = new DynamicParameters();
             parameters.Add("@productName", updateProductDto.ProductName);
             parameters.Add("@stock", updateProductDto.Stock);
